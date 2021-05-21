3cbe3ebd9f81778d2157fe11514609f4b0a26e61
malgoz <goosia1@gmail.com>
2021-05-21 21:18:28 +0200

    init project

---
+++ src/test/java/pl/kinert/malgorzata/github/GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kinert.malgorzata.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
